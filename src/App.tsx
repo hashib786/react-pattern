b785e24462b36e7a02e8ea4db047a5c2b9cd1dda
Hashib Raja <hashibraja23@gmail.com>
2023-10-19 07:54:41 +0530

    Used Hoc component in typescript ⚒️⚒️

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { ReactNode, useState } from "react";
+// import { ReactNode } from "react";
 import { faker } from "@faker-js/faker";
 import "./styles.css";
+import withToggles from "./HOC";
 
 interface ProductI {
   productName: string;
   description: string;
   price: string;
 }
-interface CompanyI {
-  companyName: string;
-  phrase: string;
-}
+// interface CompanyI {
+//   companyName: string;
+//   phrase: string;
+// }
 
 const products: ProductI[] = Array.from({ length: 20 }, () => {
   return {
@@ -20,12 +21,12 @@ const products: ProductI[] = Array.from({ length: 20 }, () => {
   };
 });
 
-const companies: CompanyI[] = Array.from({ length: 15 }, () => {
-  return {
-    companyName: faker.company.name(),
-    phrase: faker.company.catchPhrase(),
-  };
-});
+// const companies: CompanyI[] = Array.from({ length: 15 }, () => {
+//   return {
+//     companyName: faker.company.name(),
+//     phrase: faker.company.catchPhrase(),
+//   };
+// });
 
 function ProductItem({ product }: { product: ProductI }) {
   return (
@@ -37,63 +38,80 @@ function ProductItem({ product }: { product: ProductI }) {
   );
 }
 
-type CompanyItemProps = {
-  company: CompanyI;
-  defaultVisibility: boolean;
+// type CompanyItemProps = {
+//   company: CompanyI;
+//   defaultVisibility: boolean;
+// };
+
+// function CompanyItem({ company, defaultVisibility }: CompanyItemProps) {
+//   const [isVisible, setIsVisible] = useState(defaultVisibility);
+
+//   return (
+//     <li
+//       className="company"
+//       onMouseEnter={() => setIsVisible(true)}
+//       onMouseLeave={() => setIsVisible(false)}
+//     >
+//       <p className="company-name">{company.companyName}</p>
+//       {isVisible && (
+//         <p className="company-phrase">
+//           <strong>About:</strong> {company.phrase}
+//         </p>
+//       )}
+//     </li>
+//   );
+// }
+
+// type ListProp<T> = {
+//   title: string;
+//   items: Array<T>;
+//   render: (item: T) => ReactNode;
+// };
+
+// function List<T>({ title, items, render }: ListProp<T>) {
+//   const [isOpen, setIsOpen] = useState(true);
+//   const [isCollapsed, setIsCollapsed] = useState(false);
+
+//   const displayItems = isCollapsed ? items.slice(0, 3) : items;
+
+//   function toggleOpen() {
+//     setIsOpen((isOpen) => !isOpen);
+//     setIsCollapsed(false);
+//   }
+
+//   return (
+//     <div className="list-container">
+//       <div className="heading">
+//         <h2>{title}</h2>
+//         <button onClick={toggleOpen}>
+//           {isOpen ? <span>&or;</span> : <span>&and;</span>}
+//         </button>
+//       </div>
+//       {isOpen && <ul className="list">{displayItems.map(render)}</ul>}
+
+//       <button onClick={() => setIsCollapsed((isCollapsed) => !isCollapsed)}>
+//         {isCollapsed ? `Show all ${items.length}` : "Show less"}
+//       </button>
+//     </div>
+//   );
+// }
+
+// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
+export type ProductListProps = {
+  title?: string;
+  items: ProductI[];
 };
-
-function CompanyItem({ company, defaultVisibility }: CompanyItemProps) {
-  const [isVisible, setIsVisible] = useState(defaultVisibility);
-
+function ProductList({ items }: ProductListProps) {
   return (
-    <li
-      className="company"
-      onMouseEnter={() => setIsVisible(true)}
-      onMouseLeave={() => setIsVisible(false)}
-    >
-      <p className="company-name">{company.companyName}</p>
-      {isVisible && (
-        <p className="company-phrase">
-          <strong>About:</strong> {company.phrase}
-        </p>
-      )}
-    </li>
+    <ul className="list">
+      {items.map((product) => (
+        <ProductItem key={product.productName} product={product} />
+      ))}
+    </ul>
   );
 }
 
-type ListProp<T> = {
-  title: string;
-  items: Array<T>;
-  render: (item: T) => ReactNode;
-};
-
-function List<T>({ title, items, render }: ListProp<T>) {
-  const [isOpen, setIsOpen] = useState(true);
-  const [isCollapsed, setIsCollapsed] = useState(false);
-
-  const displayItems = isCollapsed ? items.slice(0, 3) : items;
-
-  function toggleOpen() {
-    setIsOpen((isOpen) => !isOpen);
-    setIsCollapsed(false);
-  }
-
-  return (
-    <div className="list-container">
-      <div className="heading">
-        <h2>{title}</h2>
-        <button onClick={toggleOpen}>
-          {isOpen ? <span>&or;</span> : <span>&and;</span>}
-        </button>
-      </div>
-      {isOpen && <ul className="list">{displayItems.map(render)}</ul>}
-
-      <button onClick={() => setIsCollapsed((isCollapsed) => !isCollapsed)}>
-        {isCollapsed ? `Show all ${items.length}` : "Show less"}
-      </button>
-    </div>
-  );
-}
+const WrappedFunction = withToggles(ProductList);
 
 export default function App() {
   return (
@@ -101,7 +119,7 @@ export default function App() {
       <h1>Render Props Demo</h1>
 
       <div className="col-2">
-        <List
+        {/* <List
           title="Products"
           items={products}
           render={(product) => (
@@ -118,19 +136,10 @@ export default function App() {
               defaultVisibility={false}
             />
           )}
-        />
+        /> */}
+        <ProductList items={products} />
+        <WrappedFunction items={products} title="Wrapped Product" />
       </div>
     </div>
   );
 }
-
-// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
-// function ProductList({ title, items }) {
-//   return (
-//     <ul className="list">
-//       {items.map((product) => (
-//         <ProductItem key={product.productName} product={product} />
-//       ))}
-//     </ul>
-//   );
-// }
--- src/HOC.tsx
+++ src/HOC.tsx
@@ -1,31 +1,34 @@
-// import { useState } from "react";
+import { useState } from "react";
+import { ProductListProps } from "./App";
 
-// export default function withToggles(WrappedComponent) {
-//   return function List(props) {
-//     const [isOpen, setIsOpen] = useState(true);
-//     const [isCollapsed, setIsCollapsed] = useState(false);
+export default function withToggles(
+  WrappedComponent: (prop: ProductListProps) => JSX.Element
+) {
+  return function List(props: ProductListProps) {
+    const [isOpen, setIsOpen] = useState(true);
+    const [isCollapsed, setIsCollapsed] = useState(false);
 
-//     const displayItems = isCollapsed ? props.items.slice(0, 3) : props.items;
+    const displayItems = isCollapsed ? props.items.slice(0, 3) : props.items;
 
-//     function toggleOpen() {
-//       setIsOpen((isOpen) => !isOpen);
-//       setIsCollapsed(false);
-//     }
+    function toggleOpen() {
+      setIsOpen((isOpen) => !isOpen);
+      setIsCollapsed(false);
+    }
 
-//     return (
-//       <div className="list-container">
-//         <div className="heading">
-//           <h2>{props.title}</h2>
-//           <button onClick={toggleOpen}>
-//             {isOpen ? <span>&or;</span> : <span>&and;</span>}
-//           </button>
-//         </div>
-//         {isOpen && <WrappedComponent {...props} items={displayItems} />}
+    return (
+      <div className="list-container">
+        <div className="heading">
+          <h2>{props.title}</h2>
+          <button onClick={toggleOpen}>
+            {isOpen ? <span>&or;</span> : <span>&and;</span>}
+          </button>
+        </div>
+        {isOpen && <WrappedComponent {...props} items={displayItems} />}
 
-//         <button onClick={() => setIsCollapsed((isCollapsed) => !isCollapsed)}>
-//           {isCollapsed ? `Show all ${props.items.length}` : "Show less"}
-//         </button>
-//       </div>
-//     );
-//   };
-// }
+        <button onClick={() => setIsCollapsed((isCollapsed) => !isCollapsed)}>
+          {isCollapsed ? `Show all ${props.items.length}` : "Show less"}
+        </button>
+      </div>
+    );
+  };
+}
